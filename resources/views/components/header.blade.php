725748ff8e1938b62d9bc594e5dafed449c61f2d
sakurairo-s <s_shibazaki_eng@outlook.jp>
2025-08-09 21:48:39 +0900

    デプロイ後の修正箇所16

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,14 +3,6 @@
        <a class="navbar-brand" href="{{ route('shops.index') }}">
        <img src="{{asset('img/logo.png')}}">
        </a>
-        <form action="{{ route('shops.index') }}" method="GET" class="row g-1">
-           <div class="col-auto">
-               <input class="form-control nagoyameshi-header-search-input" name="keyword">
-           </div>
-           <div class="col-auto">
-               <button type="submit" class="btn nagoyameshi-header-search-button"><i class="fas fa-search nagoyameshi-header-search-icon"></i></button>
-           </div>
-       </form>
        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
            <span class="navbar-toggler-icon"></span>
        </button>
