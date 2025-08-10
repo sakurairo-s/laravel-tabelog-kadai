14a70bf53918f5073d5f4ce88e3c1c0adace2815
sakurairo-s <s_shibazaki_eng@outlook.jp>
2025-08-10 15:17:51 +0900

    ぱんくず不足箇所追加2

--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -3,6 +3,13 @@
 @section('content')
 <div class="container d-flex justify-content-center mt-3">
    <div class="w-50">
+                <nav class="my-3" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb mb-0">
+            <li class="breadcrumb-item"><a href="{{ route('shops.index') }}">ホーム</a></li>
+            <li class="breadcrumb-item active" aria-current="page">新規会員登録</li>
+        </ol>
+    </nav>
+
        <h1>マイページ</h1>
 
        <hr>
